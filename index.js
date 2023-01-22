5f89fee28da445e8879f2c8149d96b905f7775f1
Amine Salhi <aminesalhi01@gmail.com>
2023-01-22 18:03:16 +0100

    completed test

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards(myArr, event){
+    let newArr = [];
+    for(let i = 0; i < myArr.length; i++){
+    let message = `Thank you, ${myArr[i]}, for the wonderful ${event} gift!`;
+    newArr.push(message);
+    }
+    console.log(newArr);
+    return newArr;
+}
+writeCards([`sam`, `carl`,`ben`], `birthday`);
+
+function countDown(){
+    let j = 10;
+    while(j >= 0){
+       console.log(j--);
+    }
+}
+console.log(countDown());
